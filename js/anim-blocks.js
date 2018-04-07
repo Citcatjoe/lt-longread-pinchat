94d8c7acbf1fe88da0f758356e78f896419b7f86
César Greppin <Citcatjoe@users.noreply.github.com>
2018-04-08 01:05:10 +0200

    Finished samedi night

--- js/anim-blocks.js
+++ js/anim-blocks.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function($) {
+
+  //JE TE CASSE LA GUEULE SALE CONNE
   var $anim1Img1 = $(".scene-stepped-1 .anim-img1");
   var $anim1Img2 = $(".scene-stepped-1 .anim-img2");
   var $anim1Img3 = $(".scene-stepped-1 .anim-img3");
@@ -8,14 +10,16 @@ jQuery(document).ready(function($) {
 
   // build tween
   var tlAnim1 = new TimelineMax()
+    .set($anim1Img1, { x: "-=40px", transformOrigin: "center center" })
     .set($anim1Img3, { scale: 0, transformOrigin: "center center" })
-    .to($anim1Img1, 1, { autoAlpha: 1, ease: Power4.easeInOut })
-    .to($anim1Img2, 1, { autoAlpha: 1, ease: Power4.easeInOut })
+    .to($anim1Img1, 1, { autoAlpha: 2, x: "0", ease: Power4.easeInOut })
     .to($anim1Img3, 0.75, {
       autoAlpha: 1,
       scale: 1,
       ease: Back.easeOut.config(5)
-    });
+    }, '-=0.5')
+    .to($anim1Img2, 4, { autoAlpha: 1, ease: Power4.easeInOut })
+    ;
 
   // build scene
   var scene = new ScrollMagic.Scene({
@@ -23,27 +27,30 @@ jQuery(document).ready(function($) {
     reverse: false
   })
     .setTween(tlAnim1)
-    .triggerHook(0.7)
-    .addIndicators({ name: "timeline" }) // add indicators (requires plugin)
+    .triggerHook(0.6)
+    // .addIndicators({ name: "timeline" }) // add indicators (requires plugin)
     .addTo(controller);
 
   //----------------------------------------------------------------------------------//
+  // JE VOUS SOUHAITE DE NE JAMAIS AVOIR D'ENFANTS
 
-  var $anim2Img1 = $(".scene-stepped-2 .anim-img1");
-  var $anim2Img2 = $(".scene-stepped-2 .anim-img2");
+  var $anim2Img1 = $(".scene-stepped-2 .anim-img2");
+  var $anim2Img2 = $(".scene-stepped-2 .anim-img1");
 
   // init controller
   var controller = new ScrollMagic.Controller();
 
   // build tween
   var tlAnim2 = new TimelineMax()
-    .set($anim2Img2, { scale: 0, transformOrigin: "center center" })
-    .to($anim2Img1, 1, { autoAlpha: 1, ease: Power4.easeInOut })
-    .to($anim2Img2, 0.75, {
+    .set($anim2Img2, { scale: 0.7, transformOrigin: "right center" })
+    .set($anim2Img1, { x: '+=40px', transformOrigin: "center center" })
+    .to($anim2Img2, 1, {
       autoAlpha: 1,
       scale: 1,
       ease: Back.easeOut.config(5)
-    });
+    })
+    .to($anim2Img1, 4, { x: '0', autoAlpha: 1, ease: Power4.easeInOut }, '-=1.5')
+    ;
 
   // build scene
   var scene = new ScrollMagic.Scene({
@@ -51,11 +58,12 @@ jQuery(document).ready(function($) {
     reverse: false
   })
     .setTween(tlAnim2)
-    .triggerHook(0.7)
-    .addIndicators({ name: "timeline" }) // add indicators (requires plugin)
+    .triggerHook(0.6)
+    // .addIndicators({ name: "timeline" }) // add indicators (requires plugin)
     .addTo(controller);
 
   //----------------------------------------------------------------------------------//
+  // JE VOIS PAS POURQUOI TRAVAILLER JE NY ARRIVERAI PAS
 
   var $anim3Img1 = $(".scene-stepped-3 .anim-img1");
   var $anim3Img2 = $(".scene-stepped-3 .anim-img2");
@@ -65,8 +73,14 @@ jQuery(document).ready(function($) {
 
   // build tween
   var tlAnim3 = new TimelineMax()
-    .to($anim3Img1, 1, { autoAlpha: 1, ease: Power4.easeInOut })
-    .to($anim3Img2, 1, { autoAlpha: 1, ease: Power4.easeInOut });
+    .set($anim3Img1, { x: '+=40px', transformOrigin: "center center" })
+    .set($anim3Img2, { scale: 0.7, transformOrigin: "left center" })
+    .to($anim3Img1, 4, { autoAlpha: 1, x: '0', ease: Power4.easeInOut })
+    .to($anim3Img2, 1, {
+      autoAlpha: 1,
+      scale: 1,
+      ease: Back.easeOut.config(3)
+    }, '-=1.5');
 
   // build scene
   var scene = new ScrollMagic.Scene({
@@ -74,29 +88,31 @@ jQuery(document).ready(function($) {
     reverse: false
   })
     .setTween(tlAnim3)
-    .triggerHook(0.7)
-    .addIndicators({ name: "timeline" }) // add indicators (requires plugin)
+    .triggerHook(0.6)
+    // .addIndicators({ name: "timeline" }) // add indicators (requires plugin)
     .addTo(controller);
 
   //----------------------------------------------------------------------------------//
+  // J'OBEIS PAS AUX FEMMES
 
   var $anim4Img1 = $(".scene-stepped-4 .anim-img1");
-  var $anim4Img2 = $(".scene-stepped-4 .anim-img2");
-  var $anim4Img3 = $(".scene-stepped-4 .anim-img3");
+  var $anim4Img2 = $(".scene-stepped-4 .anim-img3");
+  var $anim4Img3 = $(".scene-stepped-4 .anim-img2");
 
   // init controller
   var controller = new ScrollMagic.Controller();
 
   // build tween
   var tlAnim4 = new TimelineMax()
-    .set($anim4Img3, { scale: 0, transformOrigin: "center center" })
-    .to($anim4Img1, 1, { autoAlpha: 1, ease: Power4.easeInOut })
-    .to($anim4Img2, 1, { autoAlpha: 1, ease: Power4.easeInOut })
+    .set($anim4Img3, { scale: 0, transformOrigin: "left center" })
+    .set($anim4Img2, { x: '-=40px', transformOrigin: "left center" })
+    .to($anim4Img1, 3, { autoAlpha: 1, ease: Power4.easeInOut })
+    .to($anim4Img2, 2, { autoAlpha: 1, x: '0', ease: Power4.easeInOut }, '-=1.5')
     .to($anim4Img3, 0.75, {
       autoAlpha: 1,
       scale: 1,
-      ease: Back.easeOut.config(5)
-    });
+      ease: Back.easeOut.config(3)
+    }, '-=1.5');
 
   // build scene
   var scene = new ScrollMagic.Scene({
@@ -104,11 +120,12 @@ jQuery(document).ready(function($) {
     reverse: false
   })
     .setTween(tlAnim4)
-    .triggerHook(0.7)
-    .addIndicators({ name: "timeline" }) // add indicators (requires plugin)
+    .triggerHook(0.6)
+    // .addIndicators({ name: "timeline" }) // add indicators (requires plugin)
     .addTo(controller);
 
   //----------------------------------------------------------------------------------//
+  // EN PLUs TU SCHLINGUES
 
   var $anim5Img1 = $(".scene-stepped-5 .anim-img1");
   var $anim5Img2 = $(".scene-stepped-5 .anim-img2");
@@ -119,14 +136,16 @@ jQuery(document).ready(function($) {
 
   // build tween
   var tlAnim5 = new TimelineMax()
-    .set($anim5Img3, { scale: 0, transformOrigin: "center center" })
-    .to($anim5Img1, 1, { autoAlpha: 1, ease: Power4.easeInOut })
-    .to($anim5Img2, 1, { autoAlpha: 1, ease: Power4.easeInOut })
+    .set($anim5Img3, { scale: 0.7, transformOrigin: "center center" })
+    .set($anim5Img1, { x: '+=20px' })
+    .to($anim5Img1, 2, { autoAlpha: 1, x: '0', ease: Power4.easeInOut })
     .to($anim5Img3, 0.75, {
       autoAlpha: 1,
       scale: 1,
       ease: Back.easeOut.config(5)
-    });
+    }, '-=1')
+    .to($anim5Img2, 4, { autoAlpha: 1, ease: Power4.easeInOut })
+    ;
 
   // build scene
   var scene = new ScrollMagic.Scene({
@@ -134,7 +153,7 @@ jQuery(document).ready(function($) {
     reverse: false
   })
     .setTween(tlAnim5)
-    .triggerHook(0.7)
-    .addIndicators({ name: "timeline" }) // add indicators (requires plugin)
+    .triggerHook(0.6)
+    // .addIndicators({ name: "timeline" }) // add indicators (requires plugin)
     .addTo(controller);
 });
