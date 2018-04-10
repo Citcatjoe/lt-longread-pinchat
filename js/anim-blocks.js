f037d495f7385625cdf08cf4d4ce90521736797e
César Greppin <cesar.greppin@ringier.ch>
2018-04-10 11:57:37 +0200

    Changé timing animations

--- js/anim-blocks.js
+++ js/anim-blocks.js
@@ -12,13 +12,13 @@ jQuery(document).ready(function($) {
   var tlAnim1 = new TimelineMax()
     .set($anim1Img1, { x: "-=40px", transformOrigin: "center center" })
     .set($anim1Img3, { scale: 0, transformOrigin: "center center" })
-    .to($anim1Img1, 1, { autoAlpha: 2, x: "0", ease: Power4.easeInOut })
-    .to($anim1Img3, 0.75, {
+    .to($anim1Img1, 0.5, { autoAlpha: 2, x: "0", ease: Power4.easeInOut })
+    .to($anim1Img3, 0.5, {
       autoAlpha: 1,
       scale: 1,
       ease: Back.easeOut.config(5)
-    }, '-=0.5')
-    .to($anim1Img2, 4, { autoAlpha: 1, ease: Power4.easeInOut })
+    }, '-=0.25')
+    .to($anim1Img2, 2, { autoAlpha: 1, ease: Power4.easeInOut })
     ;
 
   // build scene
@@ -27,7 +27,7 @@ jQuery(document).ready(function($) {
     reverse: false
   })
     .setTween(tlAnim1)
-    .triggerHook(0.6)
+    .triggerHook(0.7)
     // .addIndicators({ name: "timeline" }) // add indicators (requires plugin)
     .addTo(controller);
 
@@ -44,12 +44,12 @@ jQuery(document).ready(function($) {
   var tlAnim2 = new TimelineMax()
     .set($anim2Img2, { scale: 0.7, transformOrigin: "right center" })
     .set($anim2Img1, { x: '+=40px', transformOrigin: "center center" })
-    .to($anim2Img2, 1, {
+    .to($anim2Img2, 0.5, {
       autoAlpha: 1,
       scale: 1,
       ease: Back.easeOut.config(5)
     })
-    .to($anim2Img1, 4, { x: '0', autoAlpha: 1, ease: Power4.easeInOut }, '-=1.5')
+    .to($anim2Img1, 1, { x: '0', autoAlpha: 1, ease: Power4.easeInOut }, '-=0.5')
     ;
 
   // build scene
@@ -58,7 +58,7 @@ jQuery(document).ready(function($) {
     reverse: false
   })
     .setTween(tlAnim2)
-    .triggerHook(0.6)
+    .triggerHook(0.7)
     // .addIndicators({ name: "timeline" }) // add indicators (requires plugin)
     .addTo(controller);
 
@@ -75,12 +75,12 @@ jQuery(document).ready(function($) {
   var tlAnim3 = new TimelineMax()
     .set($anim3Img1, { x: '+=40px', transformOrigin: "center center" })
     .set($anim3Img2, { scale: 0.7, transformOrigin: "left center" })
-    .to($anim3Img1, 4, { autoAlpha: 1, x: '0', ease: Power4.easeInOut })
-    .to($anim3Img2, 1, {
+    .to($anim3Img1, 1, { autoAlpha: 1, x: '0', ease: Power4.easeInOut })
+    .to($anim3Img2, 0.5, { 
       autoAlpha: 1,
       scale: 1,
       ease: Back.easeOut.config(3)
-    }, '-=1.5');
+    }, '-=0.5');
 
   // build scene
   var scene = new ScrollMagic.Scene({
@@ -88,7 +88,7 @@ jQuery(document).ready(function($) {
     reverse: false
   })
     .setTween(tlAnim3)
-    .triggerHook(0.6)
+    .triggerHook(0.7)
     // .addIndicators({ name: "timeline" }) // add indicators (requires plugin)
     .addTo(controller);
 
@@ -106,13 +106,13 @@ jQuery(document).ready(function($) {
   var tlAnim4 = new TimelineMax()
     .set($anim4Img3, { scale: 0, transformOrigin: "left center" })
     .set($anim4Img2, { x: '-=40px', transformOrigin: "left center" })
-    .to($anim4Img1, 3, { autoAlpha: 1, ease: Power4.easeInOut })
-    .to($anim4Img2, 2, { autoAlpha: 1, x: '0', ease: Power4.easeInOut }, '-=1.5')
-    .to($anim4Img3, 0.75, {
+    .to($anim4Img1, 1, { autoAlpha: 1, ease: Power4.easeInOut })
+    .to($anim4Img2, 1, { autoAlpha: 1, x: '0', ease: Power4.easeInOut }, '-=0.5')
+    .to($anim4Img3, 0.5, {
       autoAlpha: 1,
       scale: 1,
       ease: Back.easeOut.config(3)
-    }, '-=1.5');
+    }, '-=0.75');
 
   // build scene
   var scene = new ScrollMagic.Scene({
@@ -120,7 +120,7 @@ jQuery(document).ready(function($) {
     reverse: false
   })
     .setTween(tlAnim4)
-    .triggerHook(0.6)
+    .triggerHook(0.7)
     // .addIndicators({ name: "timeline" }) // add indicators (requires plugin)
     .addTo(controller);
 
@@ -138,13 +138,13 @@ jQuery(document).ready(function($) {
   var tlAnim5 = new TimelineMax()
     .set($anim5Img3, { scale: 0.7, transformOrigin: "center center" })
     .set($anim5Img1, { x: '+=20px' })
-    .to($anim5Img1, 2, { autoAlpha: 1, x: '0', ease: Power4.easeInOut })
-    .to($anim5Img3, 0.75, {
+    .to($anim5Img1, 1, { autoAlpha: 1, x: '0', ease: Power4.easeInOut })
+    .to($anim5Img3, 0.5, {
       autoAlpha: 1,
       scale: 1,
       ease: Back.easeOut.config(5)
-    }, '-=1')
-    .to($anim5Img2, 4, { autoAlpha: 1, ease: Power4.easeInOut })
+    }, '-=0.5')
+    .to($anim5Img2, 2, { autoAlpha: 1, ease: Power4.easeInOut })
     ;
 
   // build scene
@@ -153,7 +153,7 @@ jQuery(document).ready(function($) {
     reverse: false
   })
     .setTween(tlAnim5)
-    .triggerHook(0.6)
+    .triggerHook(0.7)
     // .addIndicators({ name: "timeline" }) // add indicators (requires plugin)
     .addTo(controller);
 });
